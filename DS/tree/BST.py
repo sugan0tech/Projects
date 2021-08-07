e95851ffc660e494714e758a446e903a4e3aab90
sugan0tech <71312159+sugan0tech@users.noreply.github.com>
2021-08-07 20:04:38 +0530

    removed

--- DS/tree/BST.py
+++
@@ -1,8 +0,0 @@
-    if tree != None:
-        inorder(tree.left)
-        print(tree.val, " ", end="")
-        inorder(tree.right)
-
-def preorder(tree, arr):
-    if tree != None:
-        arr.append(tree.val)

f1982ce2a6c7ef96c48d59b22734f5451559d6ce
sugan0tech <71312159+sugan0tech@users.noreply.github.com>
2021-08-07 20:04:30 +0530

    deleted

--- DS/tree/BST.py
+++ DS/tree/BST.py
@@ -1,14 +1,3 @@
-from binarytree import build
-import random
-
-class Node:
-    def __init__(self, value = None, right = None, left = None):
-        self.val = value
-        self.left = left
-        self.right = right
-        
-
-def inorder(tree):
     if tree != None:
         inorder(tree.left)
         print(tree.val, " ", end="")
@@ -17,31 +6,3 @@ def inorder(tree):
 def preorder(tree, arr):
     if tree != None:
         arr.append(tree.val)
-        preorder(tree.left, arr)
-        preorder(tree.right, arr)
-    return arr
-
-def create(root, data):
-    if root.val > data:
-        if root.right is None:
-            root.right = Node(data)
-        else:
-            create(root.right, data)
-    else:
-        if root.left is None:
-            root.left = Node(data)
-        else:
-            create(root.left, data)
-
-root = Node(10)
-for i in range(1, 20):
-    create(root, i)
-
-print(build(preorder(root, [])))
-
-treelst = []
-for i in range(1, 20):
-    treelst.append(i)
-binary = build(treelst)
-
-print(binary)

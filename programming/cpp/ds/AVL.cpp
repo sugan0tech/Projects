57097e9f20f6bfbac86d0ad93c611a200129de29
sugan0tech <sugankpms@gmail.com>
2021-10-17 20:27:20 +0530

    Added AVL height and depth functions

--- programming/cpp/ds/AVL.cpp
+++ programming/cpp/ds/AVL.cpp
@@ -28,7 +28,9 @@ public:
     void preorder(TreeNode *node);
     void postorder(TreeNode *node);
     void search(TreeNode *node, int num);
-    int maxHeight_Depth(TreeNode *node);
+    int height(TreeNode *node, int num);
+    int _height(TreeNode *node, int num, int &height);
+    int depth(TreeNode *node, int num);
     TreeNode *del(TreeNode *node, int num);
     TreeNode *miin(TreeNode *node);
     TreeNode *maax(TreeNode *node);
@@ -125,23 +127,37 @@ void BST::search(TreeNode *node, int num)
         cout << "\nnotfound\n";
     }
 }
-
-int BST::maxHeight_Depth(TreeNode *node)
+int BST::_height(TreeNode *node, int num, int &height)
 {
     if (node == NULL)
     {
-        return 0;
+        return -1;
     }
-    else
+    int lh = _height(node->left, num, height);
+    int rh = _height(node->right, num, height);
+    int res = max(lh, rh) + 1;
+    if (node->data == num)
     {
-        int l = maxHeight_Depth(node->left);
-        int r = maxHeight_Depth(node->right);
-        cout << node->data << l << r << endl;
-        if (l > r)
-            return l + 1;
-        else
-            return r + 1;
+        height = res;
     }
+    return res;
+}
+
+int BST::height(TreeNode *node, int num)
+{
+    int height = -1;
+    int max_height = _height(node, num, height);
+    return height;
+}
+
+int BST::depth(TreeNode *node, int num)
+{
+    if (node == NULL)
+        return -1;
+    int dist = -1;
+    if ((node->data == num) || (dist = depth(node->left, num)) >= 0 || (dist = depth(node->right, num)) >= 0)
+        return dist + 1;
+    return dist;
 }
 
 TreeNode *BST::del(TreeNode *node, int num)
@@ -204,7 +220,7 @@ int main()
 {
     BST tree;
     int opt, n = 1;
-    cout << "\n\nEnter your choice :\n 0 for exit, \n 1 for creation, \n 2 inorer ,preorder and postorder, \n 3 for insertion, \n 4 for deletion, \n 5 for searching, \n 6 For finding min and max.\n";
+    cout << "\n\nEnter your choice :\n 0 for exit, \n 1 for creation, \n 2 inorer ,preorder and postorder, \n 3 for insertion, \n 4 for deletion, \n 5 for searching, \n 6 For finding min and max,\n 7 for depth and height.\n";
     cout << " >";
     cin >> opt;
     while (opt)
@@ -253,10 +269,16 @@ int main()
         case 6:
             // min and max
             cout << "\n min : " << tree.miin(tree.root)->data << "\n max : " << tree.maax(tree.root)->data << endl;
+        case 7:
+            //depth
+            cout << "\nenter the num to see depth and height :";
+            cin >> n;
+            cout << "\n depth : " << tree.depth(tree.root, n) << endl;
+            cout << "\n height : " << tree.height(tree.root, n);
         default:
             break;
         }
-        cout << "\n\nEnter your choice :\n 0 for exit, \n 1 for creation, \n 2 inorer ,preorder and postorder, \n 3 for insertion, \n 4 for deletion, \n 5 for searching, \n 6 For finding min and max.\n";
+        cout << "\n\nEnter your choice :\n 0 for exit, \n 1 for creation, \n 2 inorer ,preorder and postorder, \n 3 for insertion, \n 4 for deletion, \n 5 for searching, \n 6 For finding min and max,\n 7 for depth and height.\n";
         cout << " >";
         cin >> opt;
     }

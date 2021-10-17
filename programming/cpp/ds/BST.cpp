661a321910db2aa91c90deba078065117bbe2948
sugan0tech <sugankpms@gmail.com>
2021-10-17 12:59:39 +0530

    altered missing functions

--- programming/cpp/ds/BST.cpp
+++ programming/cpp/ds/BST.cpp
@@ -29,7 +29,8 @@ public:
     void postorder(TreeNode *node);
     void search(TreeNode *node, int num);
     int maxHeight_Depth(TreeNode *node);
-    // TreeNode *miin(TreeNode *node);
+    TreeNode *miin(TreeNode *node);
+    TreeNode *maax(TreeNode *node);
     // TreeNode *del(TreeNode *node, int num);
 };
 
@@ -143,72 +144,117 @@ int BST::maxHeight_Depth(TreeNode *node)
     }
 }
 
-// TreeNode *miin(TreeNode *node)
-// {
-//     TreeNode *temp = node;
-//     while (temp->left != NULL)
-//     {
-//         temp = temp->left;
-//     }
-//     return temp;
-// }
+TreeNode *miin(TreeNode *node)
+{
+    TreeNode *temp = node;
+    while (temp->left != NULL)
+    {
+        temp = temp->left;
+    }
+    return temp;
+}
 
-// TreeNode *BST::del(TreeNode *node, int num)
-// {
-//     if (node == NULL)
-//         return root;
-//     else if (num < node->data)
-//         root->left = del(node->left, num);
-//     else if (num > node->data)
-//         root->right = del(node->right, num);
-//     else
-//     {
-//         if (node->left == NULL && node->right == NULL)
-//         {
-//             delete node;
-//             node = NULL;
-//         }
-//         else if (root->left == NULL)
-//         {
-//             TreeNode *temp = node;
-//             node = node->right;
-//             delete temp;
-//         }
-//         else if (root->right == NULL)
-//         {
-//             TreeNode *temp = node;
-//             node = node->left;
-//             delete temp;
-//         }
-//         else
-//         {
-//             TreeNode *temp = miin(node->right);
-//             node->data = temp->data;
-//             node->right = del(node->right, temp->data);
-//         }
-//     }
-//     return node;
-// }
+TreeNode *maax(TreeNode *node)
+{
+    TreeNode *temp = node;
+    while (temp->right != NULL)
+    {
+        temp = temp->right;
+    }
+    return temp;
+}
+TreeNode *BST::del(TreeNode *node, int num)
+{
+    if (node == NULL)
+        return root;
+    else if (num < node->data)
+        root->left = del(node->left, num);
+    else if (num > node->data)
+        root->right = del(node->right, num);
+    else
+    {
+        if (node->left == NULL && node->right == NULL)
+        {
+            delete node;
+            node = NULL;
+        }
+        else if (root->left == NULL)
+        {
+            TreeNode *temp = node;
+            node = node->right;
+            delete temp;
+        }
+        else if (root->right == NULL)
+        {
+            TreeNode *temp = node;
+            node = node->left;
+            delete temp;
+        }
+        else
+        {
+            TreeNode *temp = miin(node->right);
+            node->data = temp->data;
+            node->right = del(node->right, temp->data);
+        }
+    }
+    return node;
+}
 
 int main()
 {
     BST tree;
-    int n = 1;
-    while (n)
+    int opt, n = 1;
+    cout << "\n\nEnter your choice :\n 0 for exit, \n 1 for creation, \n 2 inorer ,preorder and postorder, \n 3 for insertion, \n 4 for deletion, \n 5 For search.\n";
+    cout << " >";
+    cin >> opt;
+    while (opt)
     {
-        cout << "enter the num : ";
-        cin >> n;
-        if (n)
+        switch (opt)
+        {
+        case 1:
+            //creation section
+            while (n != 0)
+            {
+                cout << "\nenter the num : ";
+                cin >> n;
+                if (n != 0)
+                    tree.create(n);
+            }
+            cout << "\n root node " << tree.root->data;
+            break;
+        case 2:
+            //display section
+            cout << "\n\nInorder : ";
+            tree.inorder(tree.root);
+            cout << "\n\nPreorder : ";
+            tree.preorder(tree.root);
+            cout << "\n\nPostorder : ";
+            tree.postorder(tree.root);
+            cout << endl;
+            break;
+        case 3:
+            //insertion section
+            cout << "\nenter the num : ";
+            cin >> n;
             tree.create(n);
+            break;
+        case 4:
+            //delete section
+            break;
+        case 5:
+            // search section
+            cout << "\nenter the num to search: ";
+            cin >> n;
+            tree.search(tree.root, n);
+            break;
+        case 6:
+            // min and max
+            cout << "\n min : " << miin(tree.root)->data << "\n max : " << maax(tree.root)->data << endl;
+        default:
+            break;
+        }
+        cout << "\n\nEnter your choice :\n 0 for exit, \n 1 for creation, \n 2 inorer ,preorder and postorder, \n 3 for insertion, \n 4 for deletion, \n 5 For search.\n";
+        cout << " >";
+        cin >> opt;
     }
-    cout << tree.root->data << "\n";
-    tree.inorder(tree.root);
-    cout << "\n";
-    tree.preorder(tree.root);
-    cout << "\n";
-    tree.postorder(tree.root);
-    cout << "\n";
-    tree.search(tree.root, 5);
-    cout << "\n";
-    cout << "\n Max height or Depth " << tree.maxHeight_Depth(tree.root) << endl;
 }
